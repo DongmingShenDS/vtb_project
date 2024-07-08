69829a53ec3ecbc24e51a6dd33049ad0397556ab
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-07-08 01:17:41 -0400

    update

---
+++ reward_modelling.py
@@ -0,0 +1,226 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+import evaluate
+import numpy as np
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+    PreTrainedTokenizerBase,
+    Trainer,
+    TrainerCallback,
+    TrainingArguments,
+    AutoConfig,
+    PhiForSequenceClassification
+)
+from transformers.utils import PaddingStrategy
+from trl import RewardTrainer, RewardConfig
+import wandb
+import os
+from datasets import load_metric
+from transformers import PreTrainedModel
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+import pandas as pd
+
+wandb.init(settings=wandb.Settings(init_timeout=600,
+_service_wait=600,))
+
+os.environ["WANDB__SERVICE_WAIT"] = "600"
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+
+    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
+    resume_from_checkpoint: Optional[bool] = field(
+        default=False,
+        metadata={"help": "If you want to resume training where it left off."},
+    )
+    deepspeed: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."
+        },
+    )
+    per_device_train_batch_size: Optional[int] = field(default=2)
+    per_device_eval_batch_size: Optional[int] = field(default=2)
+    gradient_accumulation_steps: Optional[int] = field(default=4)
+    learning_rate: Optional[float] = field(default=2e-5)
+    weight_decay: Optional[float] = field(default=0.001)
+    model_name: Optional[str] = field(
+        default="Qwen/Qwen1.5-0.5B",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    tokenizer_name: Optional[str] = field(
+        default="Qwen/Qwen1.5-0.5B",
+        metadata={
+            "help": "The tokenizer for your model, if left empty will use the default for your model",
+        },
+    )
+    bf16: Optional[bool] = field(
+        default=True,
+        metadata={
+            "help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."
+        },
+    )
+    num_train_epochs: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of training epochs for the reward model."},
+    )
+    # train_subset: Optional[int] = field(
+    #     default=100000,
+    #     metadata={"help": "The size of the subset of the training data to use"},
+    # )
+    eval_subset: Optional[int] = field(
+        default=-1,
+        metadata={"help": "The size of the subset of the eval data to use"},
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables gradient checkpointing."},
+    )
+    optim: Optional[str] = field(
+        default="adamw_hf",
+        metadata={"help": "The optimizer to use."},
+    )
+    lr_scheduler_type: Optional[str] = field(
+        default="linear",
+        metadata={"help": "The lr scheduler"},
+    )
+    max_length: Optional[int] = field(default=512)
+    eval_first_step: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Whether to run eval after the first step"},
+    )
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# # Load the human stack-exchange-paired dataset for tuning the reward model.
+train_dataset = pd.read_csv('../train_set.csv')
+test_dataset = pd.read_csv('../test_set.csv')
+# # if script_args.train_subset > 0:
+# #     train_dataset = train_dataset.select(range(script_args.train_subset))
+# eval_dataset = load_dataset("Anthropic/hh-rlhf", split="test")
+# Define the training args. Needs to be done before the model is loaded if you are using deepspeed.
+model_name_split = script_args.model_name.split("/")[-1]
+output_name = (
+    f"{model_name_split}_rlhf_rm_without_bsz1_{script_args.learning_rate}_{script_args.num_train_epochs}"
+)
+
+training_args = RewardConfig(
+    do_eval = True,
+    report_to="wandb",
+    output_dir=output_name,
+    learning_rate=script_args.learning_rate,
+    per_device_train_batch_size=script_args.per_device_train_batch_size,
+    per_device_eval_batch_size=script_args.per_device_eval_batch_size,
+    num_train_epochs=script_args.num_train_epochs,
+    weight_decay=script_args.weight_decay,
+    evaluation_strategy="steps",
+    eval_steps=500,
+    save_strategy="steps",
+    save_steps=500,
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    gradient_checkpointing=script_args.gradient_checkpointing,
+    deepspeed=script_args.deepspeed,
+    local_rank=script_args.local_rank,
+    remove_unused_columns=False,
+    label_names=[],
+    bf16=script_args.bf16,
+    logging_strategy="steps",
+    logging_steps=10,
+    optim=script_args.optim,
+    lr_scheduler_type=script_args.lr_scheduler_type,
+    max_length = script_args.max_length
+)
+# Load the value-head model and tokenizer.
+tokenizer_name = script_args.tokenizer_name if script_args.tokenizer_name is not None else script_args.model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_auth_token=True)
+model = AutoModelForSequenceClassification.from_pretrained(
+    script_args.model_name, torch_dtype=torch.bfloat16, num_labels=1,
+)
+
+tokenizer.pad_token = tokenizer.eos_token
+model.config.pad_token_id = tokenizer.eos_token_id
+model.config.use_cache = not script_args.gradient_checkpointing
+num_proc = 1 # Can adjust to be higher if you have more processors.
+
+
+def preprocess_function(examples):
+    new_examples = {
+            "input_ids_chosen": [],
+            "attention_mask_chosen": [],
+            "input_ids_rejected": [],
+            "attention_mask_rejected": []
+    }
+    for prompt, chosen, rejected in zip(examples["Prompt"], examples["Chosen"], examples["Reject"]):
+        chosen_str = prompt + " " + chosen
+        rejected_str = prompt + " " + rejected
+        tokenized_chosen = tokenizer(chosen_str)
+        tokenized_rejected = tokenizer(rejected_str)
+        new_examples["input_ids_chosen"].append(tokenized_chosen["input_ids"])
+        new_examples["attention_mask_chosen"].append(tokenized_chosen["attention_mask"])
+        new_examples["input_ids_rejected"].append(tokenized_rejected["input_ids"])
+        new_examples["attention_mask_rejected"].append(tokenized_rejected["attention_mask"])
+        
+    return new_examples
+
+    # Preprocess the dataset and filter out examples that are longer than args.max_length
+train_dataset = train_dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=4,
+    )
+train_dataset = train_dataset.filter(
+        lambda x: len(x["input_ids_chosen"]) <= training_args.max_length
+        and len(x["input_ids_rejected"]) <= training_args.max_length
+    )
+test_dataset = test_dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=4,
+    )
+test_dataset = test_dataset.filter(
+        lambda x: len(x["input_ids_chosen"]) <= training_args.max_length
+        and len(x["input_ids_rejected"]) <= training_args.max_length
+    )
+
+print(train_dataset)
+if script_args.eval_subset > 0:
+    eval_dataset = eval_dataset.select(range(script_args.eval_subset))
+print(eval_dataset)
+
+accuracy_metric = load_metric("accuracy")
+
+def compute_metrics(pred):
+    print(pred)
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    acc = accuracy_metric.compute(predictions=preds, references=labels)
+    return acc
+
+
+# Train the model, woohoo.
+trainer = RewardTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    compute_metrics=compute_metrics, 
+)
+
+trainer.train(script_args.resume_from_checkpoint)
+
+print("Saving last checkpoint of the model")
+model.save_pretrained(output_name + "__last_checkpoint")
